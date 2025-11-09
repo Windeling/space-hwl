2144a60ed3ea666de31293447d0cce7ec1254409
Sonata Gekko <142467582+Windeling@users.noreply.github.com>
2025-11-09 16:38:33 +0800

    Update client.ts

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -13,10 +13,9 @@ export default defineClientConfig({
 export default defineClientConfig({
   setup() {
     setupRunningTimeFooter(
-      new Date("2022-01-01"),
+      new Date("2025-11-08"),
       {
-        "/": "Running time: :day days :hour hours :minute minutes :second seconds",
-        "/zh/": "已运行 :day 天 :hour 小时 :minute 分钟 :second 秒",
+        "/": "已运行 :day 天 :hour 小时 :minute 分钟 :second 秒",
       },
       true,
     );
